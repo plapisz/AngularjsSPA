e9fa72d0c876c8a1433518e90ea37624e2b9da23
Patryk Łapisz <Patryk.Lapisz@jcommerce.pl>
2015-12-03 14:04:41 +0100

    Changed app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,25 +9,21 @@ module AppModule {
         static configureRoutes($routeProvider: ng.route.IRouteProvider) {
 
             $routeProvider
-            // route for the home page
                 .when("/", {
                     controller: "AppModule.controllers.homeController",
                     templateUrl: "app/components/home/homeView.html",
                     controllerAs: "bookCtrl"
                 })
-            //  for the customers page
                 .when("/books", {
                     controller: "AppModule.Controllers.BookController",
                     templateUrl: "app/components/books/bookView.html",
                     controllerAs: "bookCtrl"
                 })
-            // route for the contact page
                 .when("/about", {
                     controller: "AppModule.controllers.aboutController",
                     templateUrl: "app/components/about/aboutView.html",
                     controllerAs: "aboutCtrl"
                 })
-            // default redirectTo: "/"
                 .otherwise({ redirectTo: "/" });
         }
     }
