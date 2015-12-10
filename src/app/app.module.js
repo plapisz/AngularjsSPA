4ccaf47db5f907a52b06a76dc1dc336437969eb2
Patryk Łapisz <Patryk.Lapisz@jcommerce.pl>
2015-12-10 08:49:40 +0100

    Changed .gitignore and removed .js files

--- src/app/app.module.js
+++
@@ -1,6 +0,0 @@
-/// <reference path="../../typings/angularjs/angular.d.ts" />
-"use strict";
-(function () {
-    var app = angular.module("AppModule", ["ngRoute"]);
-    app.config(AppModule.Routes.configureRoutes);
-})();
